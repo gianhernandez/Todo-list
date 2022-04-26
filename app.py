334ba1eb6ebef024212df1e812ebb79dd9f4b159
gianky <gianky1@gmail.com>
2022-04-26 16:55:21 -0700

    Configure Postgresql.

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ login_manager.init_app(app)
 
 USER_DB = 'postgres'
 PASS_DB = 'admin'
-URL_DB = 'localhost'
+URL_DB = 'localhost:5432'
 NAME_DB = 'todo_list_db'
 FULL_URL_DB = f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = FULL_URL_DB
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', FULL_URL_DB)    
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 load_dotenv('env')
